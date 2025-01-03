76b561c10a643da25b1c3abcf53a3ad270430309
Yauheni Padvoiski <v-ypadvoiski@expediagroup.com>
2025-01-02 19:59:18 -0600

    add tasks

--- src/main/kotlin/com/example/Routing.kt
+++ src/main/kotlin/com/example/Routing.kt
@@ -1,13 +1,24 @@
 package com.example
 
+import com.example.model.tasks
+import com.example.model.tasksAsTable
+import io.ktor.http.ContentType
 import io.ktor.server.application.*
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
+        staticResources("/content", "mycontent", "example.html")
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/tasks") {
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = tasks.tasksAsTable())
         }
     }
 }
---
+++ src/main/kotlin/com/example/model/TaskRepository.kt
@@ -0,0 +1,8 @@
+package com.example.model
+
+val tasks = listOf(
+    Task("cleaning", "Clean the house", Priority.Low),
+    Task("gardening", "Mow the lawn", Priority.Medium),
+    Task("shopping", "Buy the groceries", Priority.High),
+    Task("painting", "Paint the fence", Priority.Medium)
+)
