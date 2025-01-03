afd424f3ffdf745de8b8a0e6d11d6d44203ca3bd
Yauheni Padvoiski <v-ypadvoiski@expediagroup.com>
2025-01-03 13:32:54 -0600

    add post handler

--- src/main/kotlin/com/example/Routing.kt
+++ src/main/kotlin/com/example/Routing.kt
@@ -1,6 +1,7 @@
 package com.example
 
 import com.example.model.Priority
+import com.example.model.Task
 import com.example.model.TaskRepository
 import com.example.model.tasksAsTable
 import io.ktor.http.ContentType
@@ -8,6 +9,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.request.receiveParameters
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -67,6 +69,35 @@ fun Application.configureRouting() {
             } catch(e: IllegalArgumentException) {
                 call.respond(HttpStatusCode.BadRequest)
             }
+        }
+
+        post("/task") {
+            val allowedParams = listOf("name", "description", "priority")
+            val formContent = call.receiveParameters()
+
+            val params = allowedParams.map { formContent[it] ?: "" }.let { (name, description, priority) ->
+                Triple(name, description, priority)
+            }
+
+            if(params.toList().any { it.isEmpty() }){
+                call.respond(HttpStatusCode.BadRequest)
+                return@post
+            }
+
+            try {
+                val ( name, description, priorityVal ) = params
+                val priority = Priority.valueOf(priorityVal)
+
+                TaskRepository.addTask(Task(name, description, priority))
+
+                call.respond(HttpStatusCode.NoContent)
+            } catch (e: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest)
+            } catch (ex: IllegalStateException) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
+
+
         }
     }
 }
