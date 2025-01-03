9e20983bb09cf537d181348a6604f292991ac1b5
Yauheni Padvoiski <v-ypadvoiski@expediagroup.com>
2025-01-03 09:18:44 -0600

    add unit tests for search route

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -4,19 +4,23 @@ import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
+import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
 import kotlin.test.assertContains
 import org.junit.Test
 import org.junit.Assert.assertEquals
 
+fun testScope (block: suspend ApplicationTestBuilder.() -> Unit): Unit = testApplication {
+    application {
+        module()
+    }
+
+    block()
+}
+
 class ApplicationTest {
-
     @Test
-    fun testRoot() = testApplication {
-        application {
-            module()
-        }
-
+    fun testRoot() = testScope {
         val response = client.get("/")
 
         assertEquals(HttpStatusCode.OK, response.status)
@@ -24,15 +28,35 @@ class ApplicationTest {
     }
 
     @Test
-    fun `should handle test1 route`() = testApplication {
-        application {
-            module()
-        }
-
+    fun `should handle test1 route`() = testScope {
         val response = client.get("/test1")
 
         assertEquals(HttpStatusCode.OK, response.status)
         assertEquals("html", response.contentType()?.contentSubtype)
         assertContains("Hello from Ktor!", response.bodyAsText())
+    }
+
+    @Test
+    fun `task should be found by priority`() = testScope {
+        val response = client.get("/tasks/byPriority/Medium")
+
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertContains(response.bodyAsText(), "Mow the lawn")
+        assertContains(response.bodyAsText(), "Paint the fence")
+    }
+
+    @Test
+    fun `should produce 400 for invalid property`() = testScope {
+        val response = client.get("/tasks/byPriority/Invalid")
+
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+    }
+
+    @Test
+    fun `should return 404 for unused property`() = testScope {
+        val response = client.get("/tasks/byPriority/Vital")
+
+        assertEquals(HttpStatusCode.NotFound, response.status)
     }
 }
