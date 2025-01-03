7ec6ffad5aa6450f3e466bae258f071e05fc68ff
Yauheni Padvoiski <v-ypadvoiski@expediagroup.com>
2025-01-02 20:37:51 -0600

    add error state exception handler for IllegalState

--- src/main/kotlin/com/example/Routing.kt
+++ src/main/kotlin/com/example/Routing.kt
@@ -7,14 +7,26 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.http.content.staticResources
+import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<IllegalStateException> { call, cause ->
+            call.respondText("App in illegal state as ${cause.message}")
+        }
+    }
+
     routing {
         staticResources("/content", "mycontent", "example.html")
+
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/error-state") {
+            throw IllegalStateException("Too Busy")
         }
 
         get("/tasks") {
