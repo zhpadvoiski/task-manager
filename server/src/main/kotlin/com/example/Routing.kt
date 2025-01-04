49867c6dacd8dd52b1de3cbadac7c5a4e7750135
yauheni padvoiski <zh.padvoiski@gmail.com>
2025-01-04 16:16:22 -0600

    add byName and byPriority to api

--- server/src/main/kotlin/com/example/Routing.kt
+++ server/src/main/kotlin/com/example/Routing.kt
@@ -9,7 +9,6 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.JsonConvertException
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
-import io.ktor.server.http.content.static
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.plugins.statuspages.StatusPages
@@ -121,6 +120,48 @@ fun Application.configureRouting() {
             route("/tasks"){
                 get {
                     call.respond(TaskRepository.allTasks())
+                }
+
+                get("/byPriority/{priority}") {
+                    val priorityParam = call.parameters["priority"]
+
+                    if(priorityParam.isNullOrEmpty()){
+                        call.respond(HttpStatusCode.BadRequest)
+                        return@get
+                    }
+
+                    try {
+                        val priority = Priority.valueOf(priorityParam)
+                        val tasks = TaskRepository.taskByPriority(priority)
+
+                        if(tasks.isEmpty()){
+                            call.respond(HttpStatusCode.NotFound)
+                            return@get
+                        }
+
+                        call.respond(tasks)
+
+                    } catch(e: IllegalArgumentException) {
+                        call.respond(HttpStatusCode.BadRequest)
+                    }
+
+                }
+
+                get("/byName/{name}") {
+                    val name = call.parameters["name"]
+
+                    if(name.isNullOrEmpty()){
+                        call.respond(HttpStatusCode.BadRequest)
+                        return@get
+                    }
+
+                    val task = TaskRepository.taskByName(name)
+                    if(task == null){
+                        call.respond(HttpStatusCode.NotFound)
+                        return@get
+                    }
+
+                    call.respond(task)
                 }
 
                 post {
